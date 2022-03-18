c260946c74bf258ccbea2cef72c818f0253c5b0a
Woxrux <shmirrakhimov@gmail.com>
2022-03-19 01:06:32 +0500

    4th version

--- script.js
+++ script.js
@@ -1,93 +1,91 @@
 'use strict';
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('How many movies have you already watched?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('How many movies have you already watched?', '');
-    }
-}
-
-start();
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt('How many movies have you already watched?', '');
+
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('How many movies have you already watched?', '');
+        }
+    },
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            let a = prompt('Name the movie you have watched recently', '');
+            if (a != null && a != '' && a.length <= 50) {
+                let b = +prompt('How would you rate it?', '');
+                if (b != null && b != '' && b <= 10 && b >= 0 || !isNaN(b)) {
+                    personalMovieDB.movies[a] = b;
+                }
+                while (b == null || b == '' || b > 10 || b < 0 || isNaN(b)) {
+                    let b = +prompt('How would you rate it?', '');
+                    if (b != null && b != '' && b <= 10 && b >= 0 && !isNaN(b)) {
+                        personalMovieDB.movies[a] = b;
+                        break;
+                    }
+                }
+            } else {
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count <= 10) {
+            prompt('Few movies watched');
+        } else if (personalMovieDB.count <= 30) {
+            prompt('You are an ordinary viewer');
+        } else if (personalMovieDB.count > 30) {
+            prompt('You are a cinephile');
+        } else {
+            prompt('Error');
+        }
+    },
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    /* toggleVisibleMyDB: function(hidden) {
+        hidden ? hidden = false : hidden = true;
+    }, */
+    toggleVisibleMyDB: function() {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+    writeYourGenres: function() {
+        // for (let i = 1; i < 4; i++) {
+        //     let genre = prompt(`Your favorite genre number ${i}`);
+
+        //     if (genre === null || genre === '') {
+        //         console.log('Incorrect answer or no at all');
+        //         i--;
+        //     } else {
+        //         personalMovieDB.genres[i-1] = genre;
+        //     }
+        // }
+        for (let i = 1; i < 2; i++) {
+            let genres = prompt(`Input your favorite genres using comma`).toLocaleLowerCase();
+
+            if (genres === null || genres === '') {
+                console.log('Incorrect answer or no at all');
+                i--;
+            } else {
+                personalMovieDB.genres = genres.split(', ');
+                personalMovieDB.genres.sort();
+            }
+        }
+        
+
+        personalMovieDB.genres.forEach((item, i) => { 
+            console.log(`Favorite genre number ${i + 1} is ${item}`);
+        });
+    }
 };
 
-
-function rememberMyFilms() {
-    // My version (BASED) (SOLVED drawback: prompt b could be skipped, but now it is fixed)
-    for (let i = 0; i < 2; i++) {
-        let a = prompt('Name the movie you have watched recently', '');
-        if (a != null && a != '' && a.length <= 50) {
-            let b = +prompt('How would you rate it?', '');
-            if (b != null && b != '' && b <= 10 && b >= 0 || !isNaN(b)) {
-                personalMovieDB.movies[a] = b;
-            }
-            while (b == null || b == '' || b > 10 || b < 0 || isNaN(b)) {
-                let b = +prompt('How would you rate it?', '');
-                if (b != null && b != '' && b <= 10 && b >= 0 && !isNaN(b)) {
-                    personalMovieDB.movies[a] = b;
-                    break;
-                }
-            }
-        } else {
-            i--;
-        }
-    }
-
-    // Author's version (drawback: in case of unacceptable answer/skip to prompt a the script does not repeat 
-    // the prompt until the fulfilling response, as in my version,
-    // but shows prompt b, after which the user will be shown that unfulfilled prompt a again)
-
-    // for (let i = 0; i < 2; i++) {
-    //     const a = prompt('Name the movie you have watched recently', ''),
-    //           b = prompt('How would you rate it?', '');
-
-    //     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-    //         personalMovieDB.movies[a] = b;
-    //         console.log('done');
-    //     } else {
-    //         console.log('error');
-    //         i--;
-    //     }
-    // }
-}
-
-rememberMyFilms();
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count <= 10) {
-        prompt('Few movies watched');
-    } else if (personalMovieDB.count <= 30) {
-        prompt('You are an ordinary viewer');
-    } else if (personalMovieDB.count > 30) {
-        prompt('You are a cinephile');
-    } else {
-        prompt('Error');
-    }
-}
-
-detectPersonalLevel();
-
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres() {
-    for (let i = 1; i < 4; i++) {
-        personalMovieDB.genres.push(prompt(`Your favorite genre number ${i}`));
-    }
-}
-
-writeYourGenres();
